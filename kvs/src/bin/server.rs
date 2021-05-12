0dc9fbedb866417c5fd2d5d007cef45c9c3234b9
ouromoros <ouromoros@gmail.com>
2021-05-12 10:34:43 +0800

    Use simple_logger

--- kvs/src/bin/server.rs
+++ kvs/src/bin/server.rs
@@ -3,6 +3,7 @@ extern crate clap;
 use clap::App;
 
 use kvs::Result;
+use log::{info};
 
 fn exit(code: i32, msg: &str) -> Result<()> {
     eprintln!("{}", msg);
@@ -10,6 +11,7 @@ fn exit(code: i32, msg: &str) -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    simple_logger::SimpleLogger::new().init().unwrap();
     let yaml = load_yaml!("server.yml");
     let matches = App::from_yaml(yaml)
         .version(env!("CARGO_PKG_VERSION"))
@@ -22,52 +24,18 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    match matches.subcommand_name() {
-        Some("get") => {
-            let matches = matches.subcommand_matches("get").unwrap();
-            let key = matches.value_of("KEY").unwrap();
-            get(key.to_owned())
-        }
-        Some("set") => {
-            let matches = matches.subcommand_matches("set").unwrap();
-            let key = matches.value_of("KEY").unwrap();
-            let value = matches.value_of("VALUE").unwrap();
-            set(key.to_owned(), value.to_owned())
-        },
-        Some("rm") => {
-            let matches = matches.subcommand_matches("rm").unwrap();
-            let key = matches.value_of("KEY").unwrap();
-            rm(key.to_owned())
-        },
-        None => exit(2, "subcommand not provided"),
-        _ => exit(3, "unsupported command"),
-    }
-}
+    let addr =  if let Some(addr) = matches.value_of("addr") {
+        addr
+    } else {
+        "127.0.0.1:4000"
+    };
+    let engine = if let Some(engine) = matches.value_of("engine") {
+        engine
+    } else {
+        "kvs"
+    };
 
-fn get(k: String) -> Result<()> {
-    let mut store = kvs::KvStore::open(&std::env::current_dir()?)?;
-    let result = store.get(k)?;
-    match result {
-        Some(v) => println!("{}", v),
-        None => println!("Key not found"),
-    }
+    info!("Addr={} Engine={}", addr, engine);
+
     Ok(())
-}
-
-fn set(k: String, v: String) -> Result<()> {
-    let mut store = kvs::KvStore::open(&std::env::current_dir()?)?;
-    store.set(k, v)
-}
-
-fn rm(k: String) -> Result<()> {
-    let mut store = kvs::KvStore::open(&std::env::current_dir()?)?;
-    let result = store.remove(k);
-    match result {
-        Ok(_) => Ok(()),
-        Err(kvs::KvsError::KeyNotExist) => {
-            println!("Key not found");
-            Err(kvs::KvsError::KeyNotExist)
-        }
-        Err(e) => Err(e)
-    }
 }
